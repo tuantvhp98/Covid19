6e18c420071d46e9d30d7ad78bbf54f37981f2d3
Tuan <tuantranvan.hp1998@gmail.com>
2020-07-30 02:16:21 +0700

    Updates

---
+++ precache-manifest.2d2fa77395479fc981be85f2da428f7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbae6806cbc79125f5ad48caa6e97c1",
+    "url": "/Covid19/index.html"
+  },
+  {
+    "revision": "7833b2db1602eb2cfaa3",
+    "url": "/Covid19/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "40b8f1c51b5f40b3fc0c",
+    "url": "/Covid19/static/css/main.712925f2.chunk.css"
+  },
+  {
+    "revision": "7833b2db1602eb2cfaa3",
+    "url": "/Covid19/static/js/2.7dfa3eb7.chunk.js"
+  },
+  {
+    "revision": "9946d09cf1d000b4a165711edf4119ce",
+    "url": "/Covid19/static/js/2.7dfa3eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40b8f1c51b5f40b3fc0c",
+    "url": "/Covid19/static/js/main.448081d3.chunk.js"
+  },
+  {
+    "revision": "cb05f9eb3d47a8af9061",
+    "url": "/Covid19/static/js/runtime-main.e4d23473.js"
+  }
+]);
